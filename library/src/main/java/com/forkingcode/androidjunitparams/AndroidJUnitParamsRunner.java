8ddf85d983b87788ea65065a5c85e3d61200fd23
Joe Rogers <joerogers@users.noreply.github.com>
2017-08-08 14:36:31 -0700

    Initial version

---
+++ library/src/main/java/com/forkingcode/androidjunitparams/AndroidJUnitParamsRunner.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Joe Rogers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.forkingcode.androidjunitparams;
+
+import android.support.test.internal.runner.junit4.statement.RunAfters;
+import android.support.test.internal.runner.junit4.statement.RunBefores;
+import android.support.test.internal.runner.junit4.statement.UiThreadStatement;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
+
+import java.util.List;
+
+import junitparams.JUnitParamsRunner;
+
+/**
+ * Specialized runner allowing UiThreadTest annotations to be honored for tests using
+ * JUnitParams.
+ *
+ * Code based on AndroidJUnit4ClassRunner. Uses internal runner logic to provide ability
+ * to honor UiThreadTest
+ */
+public class AndroidJUnitParamsRunner extends JUnitParamsRunner {
+
+    public AndroidJUnitParamsRunner(Class<?> klass) throws InitializationError {
+        super(klass);
+    }
+
+    @Override
+    protected Statement methodInvoker(FrameworkMethod method, Object test) {
+        if (UiThreadStatement.shouldRunOnUiThread(method)) {
+            return new UiThreadStatement(super.methodInvoker(method, test), true);
+        }
+        return super.methodInvoker(method, test);
+    }
+
+    @Override
+    protected Statement withBefores(FrameworkMethod method, Object target, Statement statement) {
+        List<FrameworkMethod> befores = getTestClass().getAnnotatedMethods(Before.class);
+        return befores.isEmpty() ? statement : new RunBefores(method, statement, befores, target);
+    }
+
+    @Override
+    protected Statement withAfters(FrameworkMethod method, Object target, Statement statement) {
+        List<FrameworkMethod> afters = getTestClass().getAnnotatedMethods(After.class);
+        return afters.isEmpty() ? statement : new RunAfters(method, statement, afters, target);
+    }
+}
