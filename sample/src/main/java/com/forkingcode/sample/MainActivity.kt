ec6f4a5bec138456338e092e67ce9a8ecb653e0f
Joe Rogers <joerogers@users.noreply.github.com>
2023-06-07 16:00:26 -0700

    Update R file location

--- sample/src/main/java/com/forkingcode/sample/MainActivity.kt
+++ sample/src/main/java/com/forkingcode/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.forkingcode.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.forkingcode.androidjunitparams.sample.R
 
 class MainActivity : AppCompatActivity() {
 
