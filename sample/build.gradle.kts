fe85b240602ea65c05a98cdc029dcb72bd60a780
Joe Rogers <joerogers@users.noreply.github.com>
2023-11-09 10:19:14 -0800

    Update sample to use latest compile target and AGP

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.forkingcode.androidjunitparams.sample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.forkingcode.androidjunitparams.sample"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
