00cc4ba43caee238c5c4e7f35afb67e2e38c1da0
Joe Rogers <joerogers@users.noreply.github.com>
2024-08-27 16:56:34 -0700

    Update to sdk 35, kotlin 2.0.20, and latest AS

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.forkingcode.sample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.forkingcode.androidjunitparams.sample"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -40,10 +40,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("pl.pragmatists:JUnitParams:1.1.1")
 
--- sample/src/main/java/com/forkingcode/sample/MainActivity.kt
+++ sample/src/main/java/com/forkingcode/sample/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.forkingcode.sample
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.forkingcode.sample.R.*
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.forkingcode.sample.R.layout
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(layout.activity_main)
+        val appBar: AppBarLayout = findViewById(R.id.appBar)
+
+        val paddingLeft = appBar.paddingLeft
+        val paddingRight = appBar.paddingRight
+        val paddingTop = appBar.paddingTop
+
+        ViewCompat.setOnApplyWindowInsetsListener(appBar) { view, insets ->
+
+            val bars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or
+                    WindowInsetsCompat.Type.displayCutout()
+            )
+            view.updatePadding(
+                top = paddingTop + bars.top,
+                left = paddingLeft + bars.left,
+                right = paddingRight + bars.right,
+            )
+
+            insets
+        }
     }
 }
