823ea60f81b9d14c528aeae2d205731ec65a8a77
Joe Rogers <joerogers@users.noreply.github.com>
2026-01-23 11:27:23 -0800

    Update libraries and allow APG 9.0 to run with defaults

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -45,12 +44,12 @@ kotlin {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:rules:1.7.0")
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation("pl.pragmatists:JUnitParams:1.1.1")
 
     //androidTestImplementation(project(":androidjunitparams"))
