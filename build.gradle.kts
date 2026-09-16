0b3191be617c8be38dd8d6ba16ef30fdf23de4e7
Joe Rogers <joerogers@users.noreply.github.com>
2026-09-16 11:56:58 -0700

    Update to latest kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,8 @@
 plugins {
     id("com.android.application") version "9.4.0" apply false
     id("com.android.library") version "9.4.0" apply false
+    // ADD: The Kotlin Android plugin (use your desired Kotlin 2.x version)
+    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
+
     id("com.vanniktech.maven.publish") version "0.37.0" apply false
 }
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     androidTestImplementation("pl.pragmatists:JUnitParams:1.1.1")
 
     // Kotlin
-    val kotlinBom = platform("org.jetbrains.kotlin:kotlin-bom:2.3.10")
+    val kotlinBom = platform("org.jetbrains.kotlin:kotlin-bom:2.4.20")
     implementation(kotlinBom)
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     androidTestImplementation(kotlinBom)
